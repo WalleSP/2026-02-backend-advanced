51973f68594b5346a31a251522453a11a436c8f9
zabronax <zabronax@gmail.com>
2026-01-22 10:35:34 +0100

    feat(spec): outline crud spec for asset manager

---
+++ spec/AssetManager.Spec/AssetBasics.cs
@@ -0,0 +1,28 @@
+﻿namespace AssetManager.Spec;
+
+public class AssetBasics
+{
+    [Fact]
+    public void GivenValidCreateRequest_Create_Asset()
+    {
+        // Given as valid asset create request
+        // When I want to register a new asset
+        // Then the asset is registered
+        // and I can view the update list
+    }
+
+    [Fact]
+    public void GivenAValidUpdateRequest_Update_Asset()
+    {
+    }
+
+    [Fact]
+    public void GivenAValidDeleteRequest_Update_Asset()
+    {
+    }
+
+    [Fact]
+    public void GivenAValidListRequest_Update_Asset()
+    {
+    }
+}
